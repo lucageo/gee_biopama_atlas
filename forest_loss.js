c3dc249f658f80293c6378b0a09f92d4cf6a3866
Luca Battistella <luca.battistella.geo@gmail.com>
2022-08-31 10:41:06 +0200

    Update forest_loss.js

--- forest_loss.js
+++ forest_loss.js
@@ -1,7 +1,7 @@
 var display = true; 
 var scale = 30;
-
-var ft = ee.FeatureCollection("projects/ee-biopama/assets/pas_2001");
+var year = '2011'
+var ft = ee.FeatureCollection('projects/ee-biopama/assets/pas_'+year);
 var ft_selected_prot = ft.filterMetadata('protection', 'equals', 'protected');
 var ft_selected_unprot = ft.filterMetadata('protection', 'equals', 'unprotected' );
 
@@ -10,9 +10,7 @@ var dataset = ee.Image('UMD/hansen/global_forest_change_2021_v1_9').clip(ft);
 
 
 //-----> APPLY YOUR FILTER HERE 1 = 2001 - 21 = 2021
-var dataset_year = 
-dataset.select(['lossyear']).gt(5)
-.and(dataset.select(['lossyear']).lt(12)).selfMask();
+var dataset_year = dataset.select(['lossyear']).gt(6).and(dataset.select(['lossyear']).lt(12)).selfMask();
 
 
 var areaImage = dataset_year.multiply(ee.Image.pixelArea());
@@ -35,10 +33,10 @@ var treeLossVisParam = {palette: ['red']};
 Map.addLayer(dataset_year, treeLossVisParam, 'tree loss year');
 
 
-var taskParams = { 'driveFolder' : '', 'fileFormat' : 'CSV' };
+var taskParams = { 'folder' : 'forest', 'fileFormat' : 'CSV' };
 
-Export.table(stats_prot, 'Forest_Loss_prot_test', taskParams);
-Export.table(stats_unprot, 'Forest_Loss_unprot_test', taskParams);
+Export.table(stats_prot, 'Forest_Loss_prot_'+year, taskParams);
+Export.table(stats_unprot, 'Forest_Loss_unprot_'+year, taskParams);
 
 
 
@@ -47,3 +45,4 @@ Map.addLayer(ft_selected_prot.style(style_prot), {})
 var style_unprot = {'color': 'ffffff', 'width': 1, 'lineType': 'solid', 'fillColor': 'fcb100'}
 Map.addLayer(ft_selected_unprot.style(style_unprot), {})
 var treeLossVisParam = {palette: ['red']};
+
