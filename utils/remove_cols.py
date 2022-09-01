552a8e8602fe88bd71d66d711cf1b75f414159ce
Luca Battistella <luca.battistella.geo@gmail.com>
2022-09-01 09:35:05 +0200

    Create remove_cols.py

---
+++ utils/remove_cols.py
@@ -0,0 +1,21 @@
+import os
+import pandas as pd
+directory = '/Users/lucabattistella/Downloads/atlas/forest'
+ext = ('.csv')
+
+for filename in os.listdir(directory):
+    f = os.path.join(directory, filename)
+    
+    if f.endswith(ext):
+            
+            head_tail = os.path.split(f) 
+            head_tail1 = '/Users/lucabattistella/Downloads/atlas/forest/out' 
+            k =head_tail[1] 
+            r=k.split(".")[0]
+          
+            p=head_tail1 + "/" + r + " - Output.csv" 
+
+            mydata = pd.read_csv(f)
+            
+            new =mydata[["isoa3_id_1","sum"]]
+            new.to_csv(p ,index=False)
