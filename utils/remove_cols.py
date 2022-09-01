2e1dd421cf1fb7e7bb6786251d39274fb62407da
Luca Battistella <luca.battistella.geo@gmail.com>
2022-09-01 10:21:07 +0200

    Update remove_cols.py

--- utils/remove_cols.py
+++ utils/remove_cols.py
@@ -1,6 +1,6 @@
 import os
 import pandas as pd
-directory = '/Users/lucabattistella/Downloads/atlas/forest'
+directory = '/Users/lucabattistella/Downloads/atlas/burned_areas'
 ext = ('.csv')
 
 for filename in os.listdir(directory):
@@ -9,13 +9,13 @@ for filename in os.listdir(directory):
     if f.endswith(ext):
             
             head_tail = os.path.split(f) 
-            head_tail1 = '/Users/lucabattistella/Downloads/atlas/forest/out' 
+            head_tail1 = '/Users/lucabattistella/Downloads/atlas/burned_areas/out' 
             k =head_tail[1] 
             r=k.split(".")[0]
           
-            p=head_tail1 + "/" + r + " - Output.csv" 
+            p=head_tail1 + '/' + r + ' - Output.csv'
 
             mydata = pd.read_csv(f)
             
-            new =mydata[["isoa3_id_1","sum"]]
+            new =mydata[["isoa3_id","sum"]]
             new.to_csv(p ,index=False)
