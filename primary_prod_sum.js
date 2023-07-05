a423f66afcc25e908671373c2da1bb7b37b38b92
Luca Battistella <luca.battistella.geo@gmail.com>
2023-07-05 13:51:21 +0200

    Create primary_prod_sum.js

---
+++ primary_prod_sum.js
@@ -0,0 +1,20 @@
+
+var year = '2001'
+
+var dataset = ee.ImageCollection('MODIS/006/MOD17A2H').filter(ee.Filter.date('2000-01-01', '2000-12-01')).select('Gpp');
+var ft = ee.FeatureCollection('projects/ee-biopama/assets/pas_'+year);
+
+var pp = ee.Image(dataset.reduce(ee.Reducer.sum()).clip(ft));
+
+var pp_out = pp.reduceRegions({
+  'collection': ft,
+  'reducer': ee.Reducer.sum(),
+  'scale': 500
+});
+print(pp_out);
+Export.table.toDrive({collection: pp_out, 
+                      folder: 'primaryprod',
+                      description: "pp_"+year, 
+                      fileNamePrefix: "pp_"+year,
+                      selectors:["protection","isoa3_id_1","isoa3_id ","sum"]
+                      });
