9edb4385773ec71c33f6ec07efb709410566844e
Luca Battistella <luca.battistella.geo@gmail.com>
2023-07-06 09:42:03 +0200

    Create nightlights.js

---
+++ nightlights.js
@@ -0,0 +1,45 @@
+
+
+var year = '2022'
+
+var regions_ = ee.FeatureCollection('projects/ee-biopama/assets/pas_'+year);
+var regions = regions_.filter(ee.Filter.inList('isoa3_id', ee.List(['AGO'])));
+
+//// Load nightlights image collection
+var collection = ee.ImageCollection('NOAA/VIIRS/DNB/MONTHLY_V1/VCMSLCFG');
+
+//// create list of years over which to iterate
+var years = ee.List.sequence(2014, 2022);
+
+//// Iterate through years and create a mean image per year
+var images_by_year = years.map(function(year) {
+    var filtered = collection.filter(ee.Filter.calendarRange(year, year, 'year'));
+    return filtered.mean().set('year', year);
+  });
+
+//// Make the result an Image Collection
+var mean_collection = ee.ImageCollection(images_by_year);
+
+//// Calculate mean value per boundary per image in the mean image collection
+var results = mean_collection.map(function(image) {
+  return image.reduceRegions({
+    collection: regions,
+    reducer: ee.Reducer.mean(),
+    scale: 500
+  }).map(function(f) {
+    // Add a year property to each output feature
+    return f.set('year', image.get('year'));
+  });
+});
+
+
+//// Flatten the results
+var nl_viirs = ee.FeatureCollection(results).flatten();
+print(nl_viirs)
+
+////Export the output feature collection to Google Drive as a csv file
+Export.table.toDrive(nl_viirs);
+
+
+////Add the output feature collection to the map
+Map.addLayer(nl_viirs);
